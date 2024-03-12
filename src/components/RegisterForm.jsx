83f83f03a846d1e2e398e9e302e26755c1c0f677
Venkatesan <akashvenkat47@gmail.com>
2024-03-12 19:50:09 +0530

    boilerplate react setup

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function RegisterForm(){
+    const [formValues, setFormValues] = useState({
+        name:"",
+        username:"",
+        email:"",
+        mobile:"",
+        checkbox: false,
+    });
+    const handleChange = (e) =>{
+        setFormValues({...formValues, [e.target.name]: e.target.value})
+    }
+    return (
+        <>
+            <input type="text" placeholder="Name" value={formValues.name} onChange={handleChange} name="name"/>
+            <input type="text" placeholder="UserName" value={formValues.username} onChange={handleChange} name="username"/>
+            <input type="email" placeholder="Email" value={formValues.email} onChange={handleChange} name="email"/>
+            <input type="text" placeholder="Mobile" value={formValues.mobile} onChange={handleChange} name="mobile"/>
+            <input type="checkbox" id="checkbox" name="checkbox" value={formValues.checkbox} onChange={handleChange}/>
+            <label htmlFor="checkbox">Share my registration with data with SuperApp</label>
+            <button>Sign Up</button>
+        </>
+    );
+}
